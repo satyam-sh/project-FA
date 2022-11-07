fd10b1af3a2754156a79bf06c629515f964e1542
satyam sh <satyamcvrce433@gmail.com>
2022-11-07 14:44:33 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Header from './Components/Header';
+import Header from "./Components/Header"
 import SectionOne from './Components/section1';
 import SectionThree  from './Components/section3';
 import SectionTwo from './Components/section2';
